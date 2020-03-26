8a0800f5edaa0706196df9bf68d283c99c05a3bf
matthew-paul-2006 <matthew.paul.2006@gmail.com>
2020-03-25 21:06:09 -0400

    compile the presentation

---
+++ r_course/presentations/r_code/training_help.R
@@ -0,0 +1,13 @@
+params <-
+list(isSlides = "no")
+
+## ----setup, include=FALSE-----------------------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+AsSlides <- TRUE
+
+
+## ---- eval=F------------------------------------------------------------------------------
+## install.packages("ggplot2",dependencies=TRUE)
+## install.packages("rmarkdown",dependencies=TRUE)
+## install.packages("yaml",dependencies=TRUE)
+
